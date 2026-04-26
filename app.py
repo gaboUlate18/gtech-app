7553cae8878859f1a067c46886f02a461391590e
gaboUlate18 <110076073+gaboUlate18@users.noreply.github.com>
2026-04-26 14:48:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from fpdf import FPDF
 import time
 
 # --- 1. CONFIGURACIÓN Y CONEXIÓN ---
-MONGO_URL = "TU_LINK_DE_MONGODB_AQUI"
+MONGO_URL = "mongodb+srv://gtech:Ingenieria2026@g-tech.0p52gdx.mongodb.net/?appName=G-Tech"
 
 @st.cache_resource
 def init_connection():
